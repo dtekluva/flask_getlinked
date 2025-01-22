fa0106dbd3fb41c41ec7e3f6f856b5cb513f4807
dtekluva <kboysreel@gmail.com>
2025-01-22 20:56:12 +0100

    UPDATE

--- app.py
+++ app.py
@@ -221,6 +221,6 @@ def process_images():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# if __name__ == '__main__':
-#     port = int(os.getenv("PORT", 5100))
-#     app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == '__main__':
+    # port = int(os.getenv("PORT", 5100))
+    app.run(host='0.0.0.0', debug=True)
